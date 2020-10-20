806eccfe41fb2f6898a3e9b1b4b9067657db392a
SabareeshS-eng <73077245+SabareeshS-eng@users.noreply.github.com>
2020-10-20 16:38:26 +0530

    Some comments added

--- ShapeArea.java
+++ ShapeArea.java
@@ -1,10 +1,10 @@
 import java.util.Scanner; 
-abstract class Shape{ 
-protected int a; 
-protected int b; 
-abstract void printArea(); 
+abstract class Shape{ //Abstract class to be used to print the area of a shape
+protected int a; // Data member that can be used by derived classes
+protected int b; // Data member that can be used by derived classes
+abstract void printArea(); // Method to print the area
 } 
-class Rectangle extends Shape{ 
+class Rectangle extends Shape{ // Used to print the area of the rectangle
 Scanner input=new Scanner(System.in); 
 @Override 
 void printArea() { 
@@ -15,7 +15,7 @@ int area=a*b;
 System.out.println("Area of rectangle with length "+a+" and breadth "+b+" is "+area); 
 } 
 } 
-class Triangle extends Shape{ 
+class Triangle extends Shape{ // Used to print the area of the Triangle
 int c; 
 Scanner input=new Scanner(System.in); 
 @Override 
@@ -29,7 +29,7 @@ double area=Math.pow(s*(s-a)*(s-b)*(s-c),0.5);
 System.out.println("Area of the triangle is "+area); 
 } 
 } 
-class Circle extends Shape{ 
+class Circle extends Shape{ // Used to print the area of the circle
 Scanner input=new Scanner(System.in); 
 @Override 
 void printArea() { 
@@ -38,7 +38,7 @@ a= input.nextInt();
 double area=3.14*a*a; 
 System.out.println("Area of circle is "+area); 
 } }
-public class ShapeArea { 
+public class ShapeArea { // This class contains main method from which the program starts
 public static void main(String[] args){ 
 Shape r=new Rectangle(); 
 r.printArea(); 
